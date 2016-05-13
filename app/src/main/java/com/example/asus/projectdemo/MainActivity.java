7eb1debc4b11180e63f86816b69cf211690b7a32
Thang Duong <duongnhatthang@gmail.com>
2016-05-13 23:38:52 +0700

    -Repair all layout errors. -Add reset color when user click on the screen.

--- app/src/main/java/com/example/asus/projectdemo/MainActivity.java
+++ app/src/main/java/com/example/asus/projectdemo/MainActivity.java
@@ -7,24 +7,25 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    public static final int NUM_OF_COLLUMN = 5;
-    public static final int NUM_OF_ROW = 6;
-    private GridView gridView;
+    public static final int NAM_OF_COLUMN = 13;
+    public static final int NUM_OF_ROW = 13;
+    private static GridView gridView;
     private TextView txtView_question;
     private ImageView imgView_question;
     private Context context;
     private KeyboardButton[] keyboard_btn;
-    private String [][] gridViewData = new String[NUM_OF_COLLUMN][NUM_OF_ROW];//gridViewData[x][y]
+    private String [][] gridViewData = new String[NAM_OF_COLUMN][NUM_OF_ROW];//gridViewData[x][y]
     private WordObjectsManager objManger = WordObjectsManager.getInstance();
     private GridviewAdapter adapter;
 
-    private TextView testKeyboard;
+//    private TextView testKeyboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +33,38 @@ public class MainActivity extends Activity {
         context = this;
 
         setupGridView();
-        adapter = new GridviewAdapter(getApplicationContext(), gridViewData);
         gridView = (GridView) findViewById(R.id.layout_gridview);
+        adapter = new GridviewAdapter(getApplicationContext(), gridViewData);
         gridView.setAdapter(adapter);
-        gridView.setNumColumns(NUM_OF_COLLUMN);
+        gridView.setNumColumns(NAM_OF_COLUMN);
+        gridView.setColumnWidth((int) ((gridView.getWidth() / NAM_OF_COLUMN) * 0.9));
+        //reset color onclick
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                adapter.resetColor();
+            }
+        });
+//        gridView.sethei(gridView.getWidth()/NAM_OF_COLUMN);
 
         setupKeyboard();
         setOnTouchKeyboard();
-        testKeyboard = (TextView)findViewById(R.id.testKeyboard);
+        /*testKeyboard = (TextView)findViewById(R.id.testKeyboard);
         testKeyboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 testKeyboard.setText("");
             }
-        });
+        });*/
 
         txtView_question = (TextView)findViewById(R.id.textView);
         imgView_question = (ImageView)findViewById(R.id.imageView);
 
+    }
+
+    public static int getRowHeight()
+    {
+        return (int)((gridView.getHeight() / NUM_OF_ROW)*0.9);
     }
 
     private void setupKeyboard()
@@ -88,6 +103,9 @@ public class MainActivity extends Activity {
         for(int i = 0;i<keyboard_btn.length;i++)
         {
             setOnClickEachButton(keyboard_btn[i]);
+            //Setup size of each cell
+//            keyboard_btn[i].button.setWidth(gridView.getWidth()/NAM_OF_COLUMN);
+//            keyboard_btn[i].button.setHeight(gridView.getHeight()/NUM_OF_ROW);
         }
 //        adapter.notifyDataSetChanged();
     }
@@ -97,7 +115,7 @@ public class MainActivity extends Activity {
         kbtn.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                testKeyboard.setText(testKeyboard.getText() + kbtn.key);
+//                testKeyboard.setText(testKeyboard.getText() + kbtn.key);
                 try {
                     gridViewData[WordObject.getClickedPositionX()][WordObject.getClickedPositionY()]=kbtn.key;
                     adapter.nextClickedPosition();
@@ -116,8 +134,11 @@ public class MainActivity extends Activity {
         objManger.add(new WordObject(0,0,"Test Question 1","aaa1",WordObject.HORIZONTAL));
         objManger.add(new WordObject(3,2,"Test Question 2","bb2",WordObject.VERTICAL));
         objManger.add(new WordObject(0,4,"Test Question 3","cccc3",WordObject.HORIZONTAL));
-        objManger.add(new WordObject(1,2,"Test Question 1","zz4",WordObject.HORIZONTAL));
-        objManger.add(new WordObject(1,0,"Test Question 1","pp4",WordObject.VERTICAL));
+        objManger.add(new WordObject(1,2,"Test Question 4","zz4",WordObject.HORIZONTAL));
+        objManger.add(new WordObject(1,0,"Test Question 5","pp4",WordObject.VERTICAL));
+
+        objManger.add(new WordObject(0,12,"Test Question 6","1234567891234",WordObject.HORIZONTAL));
+        objManger.add(new WordObject(12,0,"Test Question 7","1234567891234",WordObject.VERTICAL));
     }
     private void setupGridView()
     {
